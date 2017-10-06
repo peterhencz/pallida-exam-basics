9dbd7ef8a9a25be0dd96be660f06df49f31f8370
garamreka <garam.reka@gmail.com>
2017-10-06 11:53:48 +0200

    able to add one animal

---
+++ favouriteanimals/FavouriteAnimals/Animals.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavouriteAnimals
+{
+    class Animals
+    {
+        static string path = @"../../favourites.txt";
+        string[] contentFavourites = File.ReadAllLines(path);
+
+        public void UsageInfo()
+        {
+            Console.WriteLine("C# FavouriteAnimals [animal] [animal]");
+        }
+
+        public void ListAnimals ()
+        {
+            for (int i = 0; i < contentFavourites.Length; i++)
+            {
+                Console.WriteLine(contentFavourites[i]);
+            }
+        }
+
+        public void AddAnimals (string[] args)
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(path))
+                {
+                    writer.WriteLine(args[0]);
+
+                    //for (int i = 0; i < args.Length; i++)
+                    //{
+                    //    writer.WriteLine(args[i]);
+                    //}
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Unable to add");
+            }
+        }
+    }
+}
