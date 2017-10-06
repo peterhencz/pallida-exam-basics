b15946006c3c7f4c3df39f14016e0a7a4902b82f
garamreka <garam.reka@gmail.com>
2017-10-06 10:08:36 +0200

    whenAnagram thenNgrm test passed

---
+++ uniquechars/UniqueChars/UniqueCharacter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueChars
+{
+    public class UniqueCharacter
+    {
+        // Create a method called `UniqueCharacters` that takes a string as parameter
+        // and returns a list with the unique letters of the given string
+        // Create basic unit tests for it with at least 3 different test cases
+        // Print the characters from that list in the following format:
+        // "n", "g", "r", "m"
+
+        public List<char> UniqueCharacters (string input)
+        {
+            var uniqueList = new List<char>();
+
+            char[] characters = input.ToCharArray();
+            var countChars = new Dictionary<char, int>();
+
+            if (input != null)
+            {
+                foreach (var character in characters)
+                {
+                    if (!countChars.ContainsKey(character))
+                    {
+                        countChars.Add(character, 1);
+                    }
+                    else
+                    {
+                        countChars[character]++;
+                    }
+                }
+
+                foreach (var pair in countChars)
+                {
+                    if (pair.Value == 1)
+                    {
+                        uniqueList.Add(pair.Key);
+                    }
+                }
+            }
+
+            return uniqueList;
+        }
+    }
+}
---
+++ uniquechars/UniqueCharsTest/UniqueCharactersTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueChars;
+
+namespace UniqueCharsTest
+{
+    [TestFixture]
+    public class UniqueCharactersTest
+    {
+        UniqueCharacter uniqueCharacter = new UniqueCharacter();
+
+        [Test]
+        public void AddToList_WhenAnagram_ThenNgrm ()
+        {
+            string input = "anagram";
+            var output = new List<char>() { 'n', 'g', 'r', 'm' };
+            Assert.AreEqual(output, uniqueCharacter.UniqueCharacters(input));
+        }
+    }
+}
