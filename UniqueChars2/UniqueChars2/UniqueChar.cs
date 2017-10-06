5660d3d7d1db4fb532f1567c83b5cdb1c517728e
garamreka <garam.reka@gmail.com>
2017-10-06 10:56:43 +0200

    complete first exercise in UniqueChars2 without printing

---
+++ UniqueChars2/UniqueChars2/UniqueChar.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniqueChars2
+{
+    public class UniqueChar
+    {
+        public List<char> UniqueCharacters(string input)
+        {
+            var uniqueList = new List<char>();
+            char[] characters = input.ToCharArray();
+            var countChars = new Dictionary<char, int>();
+
+            foreach (var character in characters)
+            {
+                if (!countChars.ContainsKey(character))
+                {
+                    countChars.Add(character, 1);
+                }
+                else
+                {
+                    countChars[character]++;
+                }
+            }
+
+            foreach (var pair in countChars)
+            {
+                if (pair.Value == 1 && pair.Key != ' ')
+                {
+                    uniqueList.Add(pair.Key);
+                }
+            }
+
+            return uniqueList;
+        }
+    }
+}
---
+++ UniqueChars2/UniqueCharsTest2/UniqueCharTest.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueChars2;
+
+namespace UniqueCharsTest2
+{
+    [TestFixture]
+    public class UniqueCharTest
+    {
+        UniqueChar uniqueCharacter = new UniqueChar();
+
+        [Test]
+        public void AddToList_WhenAnagram_ThenNgrm()
+        {
+            string input = "anagram";
+            var output = new List<char>() { 'n', 'g', 'r', 'm' };
+            Assert.AreEqual(output, uniqueCharacter.UniqueCharacters(input));
+        }
+
+        [Test]
+        public void AddToList_WhenEmptyString_ThenEmptyList()
+        {
+            string input = string.Empty;
+            var output = new List<char>();
+            Assert.AreEqual(output, uniqueCharacter.UniqueCharacters(input));
+        }
+
+        [Test]
+        public void AddToList_WhenAnagramUpperCase_ThenAngrm()
+        {
+            string input = "Anagram";
+            var output = new List<char>() { 'A', 'n', 'g', 'r', 'm' };
+            Assert.AreEqual(output, uniqueCharacter.UniqueCharacters(input));
+        }
+
+        [Test]
+        public void AddToList_WhenTwoWords_ThenWhiteSpaceDontCount()
+        {
+            string input = "alpaga pallida";
+            var output = new List<char>() { 'g', 'i', 'd' };
+            Assert.AreEqual(output, uniqueCharacter.UniqueCharacters(input));
+        }
+
+    }
+}
