d9921eeeca928dcfaf74a46f7d3543a4a275557b
garamreka <garam.reka@gmail.com>
2017-10-06 12:16:44 +0200

    unable to not to add existing animals come back if have time

--- favouriteanimals/FavouriteAnimals/Animals.cs
+++ favouriteanimals/FavouriteAnimals/Animals.cs
@@ -1,38 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FavouriteAnimals
 {
     class Animals
     {
-        static string path = @"../../favourites.txt";
-        string[] contentFavourites = File.ReadAllLines(path);
+        static string pathToFavourites = @"../../favourites.txt";
+        string[] contentOfFavourites = File.ReadAllLines(pathToFavourites);
 
         public void UsageInfo()
         {
             Console.WriteLine("C# FavouriteAnimals [animal] [animal]");
         }
 
-        public void ListAnimals ()
-        {
-            for (int i = 0; i < contentFavourites.Length; i++)
-            {
-                Console.WriteLine(contentFavourites[i]);
-            }
-        }
-
         public void AddAnimals (string[] args)
         {
             try
             {
-                using (StreamWriter writer = File.AppendText(path))
+                using (StreamWriter writer = File.AppendText(pathToFavourites))
                 {
                     for (int i = 0; i < args.Length; i++)
                     {
+                        // TODO
                         writer.WriteLine(args[i]);
                     }
                 }
